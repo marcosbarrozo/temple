1a3c2dee50a93febb53977eb2186690522d86989
marcosbarrozo <104003734+marcosbarrozo@users.noreply.github.com>
2022-07-23 09:03:33 -0600

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -6,7 +6,21 @@ const currentTemp = document.querySelector('#temperature');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('#condition');
 const humidity = document.querySelector('#humidity');
-Bethesda,
+
+const dayOneTemp = document.querySelector('#day1-temperature');
+const dayOneWeatherIcon = document.querySelector('#day1-weather-icon');
+const dayOneCaptionDesc = document.querySelector('#day1-condition');
+const dayOneHumidity = document.querySelector('#day1-humidity');
+
+const dayTwoTemp = document.querySelector('#day2-temperature');
+const dayTwoWeatherIcon = document.querySelector('#day2-weather-icon');
+const dayTwoCaptionDesc = document.querySelector('#day2-condition');
+const dayTwoHumidity = document.querySelector('#day2-humidity');
+
+const dayThreeTemp = document.querySelector('#day3-temperature');
+const dayThreeWeatherIcon = document.querySelector('#day3-weather-icon');
+const dayThreeCaptionDesc = document.querySelector('#day3-condition');
+const dayThreeHumidity = document.querySelector('#day3-humidity');
 const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=39&lon=-77.04&exclude=hourly,minutely&appid=d26cadee2be0b0548e24934fba75e01f&units=imperial';
 
 async function apiFetch() {
@@ -29,14 +43,48 @@ async function apiFetch() {
   function  displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.current.temp.toFixed(0)}</strong>`;
   
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.current.weather[0].icon}.png`;
-    const desc = weatherData.current.weather[0].description;
-    const humidityDesc = weatherData.current.humidity;
+    let iconsrc = `https://openweathermap.org/img/w/${weatherData.current.weather[0].icon}.png`;
+    let desc = weatherData.current.weather[0].description;
+    let humidityDesc = weatherData.current.humidity;
     
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
     humidity.textContent = humidityDesc
+
+    dayOneTemp.innerHTML = `<strong>${weatherData.daily[0].temp['day'].toFixed(0)}</strong>`;
+  
+    iconsrc = `https://openweathermap.org/img/w/${weatherData.daily[0].weather[0].icon}.png`;
+    console.log(weatherData.daily[0].weather[0].icon)
+    desc = weatherData.daily[0].weather[0].description;
+    humidityDesc = weatherData.daily[0].humidity;
+    
+    dayOneWeatherIcon.setAttribute('src', iconsrc);
+    dayOneWeatherIcon.setAttribute('alt', desc);
+    dayOneCaptionDesc.textContent = desc;
+    dayOneHumidity.textContent = humidityDesc
+
+    dayTwoTemp.innerHTML = `<strong>${weatherData.daily[1].temp['day'].toFixed(0)}</strong>`;
+  
+    iconsrc = `https://openweathermap.org/img/w/${weatherData.daily[1].weather[0].icon}.png`;
+    desc = weatherData.daily[0].weather[0].description;
+    humidityDesc = weatherData.daily[0].humidity;
+    
+    dayTwoWeatherIcon.setAttribute('src', iconsrc);
+    dayTwoWeatherIcon.setAttribute('alt', desc);
+    dayTwoCaptionDesc.textContent = desc;
+    dayTwoHumidity.textContent = humidityDesc
+
+    dayThreeTemp.innerHTML = `<strong>${weatherData.daily[2].temp['day'].toFixed(0)}</strong>`;
+  
+    iconsrc = `https://openweathermap.org/img/w/${weatherData.daily[2].weather[0].icon}.png`;
+    desc = weatherData.daily[0].weather[0].description;
+    humidityDesc = weatherData.daily[0].humidity;
+    
+    dayThreeWeatherIcon.setAttribute('src', iconsrc);
+    dayThreeWeatherIcon.setAttribute('alt', desc);
+    dayThreeCaptionDesc.textContent = desc;
+    dayThreeHumidity.textContent = humidityDesc
 
 
 
