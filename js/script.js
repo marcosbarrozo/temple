97c6df7c10cebf70f90be6f1dabde7d8474924b7
marcosbarrozo <104003734+marcosbarrozo@users.noreply.github.com>
2022-07-25 11:28:25 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu-icon");
+const closeBtn = document.querySelector("#close-btn");
 menu.addEventListener("click", function(){
     if(navbar.classList.contains("show")){
         navbar.classList.remove("show");
@@ -7,3 +8,12 @@ menu.addEventListener("click", function(){
         navbar.classList.add("show");
     }
 });
+
+closeBtn.addEventListener("click", function(){
+    if(navbar.classList.contains("show")){
+        navbar.classList.remove("show");
+    }
+});
+
+const last_updated = document.querySelector(".last_updated");
+last_updated.textContent = document.lastModified;
